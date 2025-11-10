306316459bfec49c685ba06ea986daa924379eaf
juan quintero <codelatincolombia@gmail.com>
2025-11-10 12:00:24 -0500

    agregando al servidor

--- repo_estudiantil/settings.py
+++ repo_estudiantil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q-eelre&7e*d*)#nn4h^l50oeharyzyeivn*bl8nrgi*zu^_a=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
